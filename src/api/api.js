86dc039de819ca00a1b3e144a6df47adf4d57e35
lironzaa <lironzaa@gmail>
2020-01-15 02:07:42 +0200

    working on validation

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 const baseURL = "https://itunes.apple.com/";
 
 export const searchItem = searchedQuery => {
-  console.log(searchedQuery);
   return axios.post(`${baseURL}search?term=${searchedQuery}&limit=25`)
     .then(searchResults => {
       return searchResults.data;
--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -9,8 +9,19 @@ class Main extends Component {
     this.state = {
       searchedQuery: '',
       searchResults: [],
-      errors: {}
+      resultCount: null,
+      error: ''
     }
+  }
+
+  validate = () => {
+    console.log(this.state.searchedQuery.length);
+    if (this.state.searchedQuery.length === 0) {
+      console.log('test2');
+      this.setState({ error: 'Searched song/movie must contain at least 1 character' });
+      return false;
+    }
+    return true;
   }
 
   onChange = e => {
@@ -19,14 +30,32 @@ class Main extends Component {
 
   onSubmit = e => {
     e.preventDefault();
-    searchItem(this.state.searchedQuery).then(({ results, resultCount }) => {
-      this.setState({ searchResults: results });
-      console.log(this.state.searchResults);
-    });
+    const isValid = this.validate();
+    if (isValid) {
+      searchItem(this.state.searchedQuery).then(({ results, resultCount }) => {
+        this.setState({ searchResults: results, resultCount });
+        console.log(this.state.searchResults);
+      });
+    }
   }
 
   render() {
-    const { errors } = this.state;
+    const { error, searchResults, resultCount } = this.state;
+    const searchItems = searchResults.length ? (
+      searchResults.map(item => {
+        return (
+          <div className="col-lg-2 col-md-6 col-sm-10 card mr-4 mb-5" key={item.trackId}>
+            <img className="card-img-top" src={item.artworkUrl100} alt={item.trackName}></img>
+            <div className="card-body">
+              <h5 className="card-title">{item.trackName}</h5>
+              <p className="card-text"><span className="font-weight-bold">Type :</span> {item.kind}</p>
+              <p className="card-text"><span className="font-weight-bold">Artist :</span> {item.artistName}</p>
+              <p className="card-text"><span className="font-weight-bold">Album :</span> {item.collectionName}</p>
+            </div>
+          </div>
+        )
+      })
+    ) : resultCount === null ? <div className="mx-auto">Please search for a song/movie</div> : <div className="mx-auto">No results for this search</div>;
 
     return (
       <div className="container main">
@@ -42,13 +71,16 @@ class Main extends Component {
               <div className="form-group">
                 <input type="text" value={this.state.searchedQuery}
                   className={classnames("form-control form-control-lg", {
-                    'is-invalid': errors.searchedQuery
+                    'is-invalid': error
                   })} onChange={this.onChange} placeholder="Search" name="searchedQuery" />
-                {errors.searchedQuery && (<div className="invalid-feedback">{errors.searchedQuery}</div>)}
+                {error && (<div className="invalid-feedback">{error}</div>)}
               </div>
               <input type="submit" value="Search" className="btn btn-info btn-block mt-4" />
             </form>
           </div>
+        </div>
+        <div className="row mt-5">
+          {searchItems}
         </div>
       </div>
     )
