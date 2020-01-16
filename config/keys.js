bce04f4aedf04a2c87dcf4d27366145c45fb1c20
lironzaa <lironzaa@gmail>
2020-01-17 00:49:53 +0200

    backend register&login

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://lironzaa:liroN123@anyvision-itunes-599oa.mongodb.net/test?retryWrites=true&w=majority',
+  secretOrKey: 'dasdaskidsaudasjudjsa90dsaodasjkewquiejqwuiejwq2'
+}
--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const auth = require('./routes/api/auth');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send('API running'));
+
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  .then(() => console.log('mongoDB connect'))
+  .catch(err => console.log(err));
+
+app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const PORT = process.env.PORT || 5000;
 
